85e606a36870b49074e80fa88f001886abf12679
walerij <becwal@yandex.ru>
2018-08-26 21:48:00 +0300

    Создание сайта социальной сети test_net на основе фреймворка Yii2

---
+++ views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Социальная сеть testnet. Версия 0.0.0.1alpha
+    </p>
+
+    
+</div>
